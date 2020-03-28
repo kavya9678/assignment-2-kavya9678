b5cc74300f6c3fee689de7e8646ae0deeb24ea7e
Kavya Singh <44774092+kavya9678@users.noreply.github.com>
2020-03-28 18:01:03 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
-// executable class
-
+import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(100);
+        bst.insert(30);
+        bst.insert(45);
+        bst.insert(15);
+        bst.insert(17);
+        bst.insert(15);
+        bst.insert(10);
+        bst.insert(5);
+        bst.diaplay(bst.getRoot(), bst.getMlevel());
+        bst.d2();
+
 }
