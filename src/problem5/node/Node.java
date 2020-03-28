de2faa7345160b25853c14ca9f75eeca48d51c9e
Kavya Singh <44774092+kavya9678@users.noreply.github.com>
2020-03-28 18:48:55 +0530

    Update Node.java

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,25 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
-// to define node properties
+import problem5.student.Student;
 public class Node {
+    Student student;
+    Node next;
+    public Node(Student student) {
+        this.student = student;
+        this.next = null;
+
+    }
+    public Student getStudent() {
+        return student;
+    }
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
