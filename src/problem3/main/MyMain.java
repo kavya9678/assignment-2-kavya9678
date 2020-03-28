13e5cdbbf0cd4471d4981d7b0f1e0933b0ef3c83
Kavya Singh <44774092+kavya9678@users.noreply.github.com>
2020-03-28 18:12:34 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,10 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
-// executable class
-// use problem5.student.Student class to create object of student
+import problem3.myqueue.MyPriorityQueue;
 public class MyMain {
+  MyPriorityQueue pq = new MyPriorityQueue();
+        pq.push("a", 1);
+        pq.push("b", 2);
+        pq.push("c", 3);
 }
